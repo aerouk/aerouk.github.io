f141fe8a3b9b3d518dfcfe11f93fa0796a47c9ec
iEpic <xcraaze@gmail.com>
2013-08-04 13:31:11 +0100

    updated some files

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>AeroPvP &sdot; Home</title>
+	<meta name="description" content="Welcome to AeroPvP's website. This is where I'll post information about what I'm doing, who I am and other stuff.">
+	<meta name="keywords" content="aeropvp, aero, pvp, kingspvp, aeropvp kingspvp, aeropvp texture pack, aeropvp texture, minecraftpvp, thekingspvp">
+	<meta name="robot" content="index,follow">
+	<meta name="language" content="en">
+	<meta name="revisit-after" content="1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" href="/inc/img/favicon.png" type="image/png">
+	<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet">
+</head>
+<body>
+
+	<div class="navbar navbar-static-top navbar-inverse">
+		<div class="navbar-inner">
+			<div class="container">
+				<button type="button" class="btn btn-navbar collapsed" data-toggle="collapse" data-target=".nav-collapse">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="brand" href="/">AeroPvP</a>
+				<div class="nav-collapse collapse" style="height: 0px;">
+					<ul class="nav">
+						<li class="active"><a href="/">Home</a></li>
+						<li class="divider-vertical"></li>
+						<li><a href="/reports">Reports</a></li>
+						<li class="divider-vertical"></li>
+						<li><a href="/about">About me</a></li>
+						<li class="divider-vertical"></li>
+						<li><a href="/colors.php">MC Colors</a></li>
+						<li class="divider-vertical"></li>
+					</ul>
+					<ul class="nav pull-right">
+						<li class="divider-vertical"></li>
+						<li><a href="//minecraftpvp.com">Minecraft PVP Servers</a></li>
+						<li class="divider-vertical"></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container">
+
+		<br>
+		<p class="text-center"><strong>It might take me a while to get this set up. For now, here's a dancing penguin.</strong></p>
+		<center><img src="http://kpvpdev.us/img/penguin.gif"/></center>
+
+		<hr>
+		<footer class="footer">
+			<p class="pull-right"><a href="#">Top <i class="icon-arrow-up"></i></a></p>
+			<p>Made by <a href="http://twitter.com/AeroMCPVP" target="_blank">Aero</a> <small>using <a href="//twitter.github.io/bootstrap" target="_blank">Twitter Bootstrap</a> and <a href="//glyphicons.com" target="_blank">Glyphicons</a>.</small><br><i class="icon-envelope"></i> <a href="mailto:xaero.pvp@gmail.com">xaero.pvp@gmail.com</a></p>
+		</footer>
+		<hr>
+
+	</div>
+
+	</div>
+
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+	<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		
+		ga('create', 'UA-41054573-1', 'iaero.me');
+		ga('send', 'pageview');
+	</script>
+
+</body>
+</html>
